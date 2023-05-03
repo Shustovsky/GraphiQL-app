dd0cdd708feef53822f10c0a050a139ea5737418
A. Shustovsky <a.shustovsky@gmail.com>
2023-05-03 11:08:29 +0400

    feat: add sticky header init: start project from Vite

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import logo from '../assets/icons/logo.png';
+import { useEffect, useState } from 'react';
+import { Button } from './Button';
+
+export function Header(): JSX.Element {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsSticky(scrollTop > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={`py-1 px-10 flex justify-between sticky top-0 transition-all duration-700 ${
+        isSticky ? 'h-12 ' : 'h-14 bg-black'
+      }`}
+    >
+      <img className="w-10 h-10" src={logo} alt="logo" />
+
+      <Button
+        label="Sign up"
+        onClick={() => {
+          /*TODO заглушка на кнопку*/
+        }}
+      />
+    </header>
+  );
+}
