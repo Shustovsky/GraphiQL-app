8a694f15ea5c994e4cee7a7af9ddb7b1f631331b
D1van007 <d1van28.01.1987@gmail.com>
2023-05-16 00:33:05 +0300

    feat: add GraphQLClient and try write request

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,6 +9,7 @@ import play from '../assets/icons/play.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { Loader } from '../components/loader/Loader';
 import { setLoading } from '../store/slices/loadingSlice';
+import { GraphQLClient, gql } from 'graphql-request';
 
 export default function MainPage() {
   const dispatch = useDispatch();
@@ -26,6 +27,7 @@ export default function MainPage() {
       '  }\n' +
       '}'
   );
+  const [textAreaVariable, setTextAreaVariable] = useState<string>('');
   const [responseText, setResponseText] = useState<string>(
     'Press the Play Button to get a response here'
   );
@@ -36,6 +38,10 @@ export default function MainPage() {
 
   const changeTextAreaHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextAreaValue(event.target.value);
+  };
+
+  const changeTextAreaVarHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextAreaVariable(event.target.value);
   };
 
   const makeRequest = (query): Promise<void> => {
@@ -54,6 +60,29 @@ export default function MainPage() {
     );
   };
 
+  const makeRequestWithLibrary = (query) => {
+    const client = new GraphQLClient(inputValue);
+    console.log(client);
+
+    const queryGQL = gql`
+      ${query}
+    `;
+
+    const variables = JSON.parse(textAreaVariable);
+
+    const requestHeaders = {
+      'Content-type': 'application/json',
+    };
+
+    // Overrides the clients headers with the passed values
+    const datac = client
+      .request(queryGQL, variables, requestHeaders)
+      .then((data) => console.log(data));
+    console.log(datac);
+
+    // setResponseText(data);
+  };
+
   useEffect(() => {
     console.log(db);
     if (loading) return;
@@ -61,7 +90,7 @@ export default function MainPage() {
   }, [user, loading, navigate]);
 
   return (
-    <main className="bg-[#002B36] text-white">
+    <main className="bg-[#002B36] text-white h-[85vh]">
       <input
         className="bg-[#002B36] text-black placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
         placeholder="Search for anything..."
@@ -71,22 +100,30 @@ export default function MainPage() {
         onChange={changeInputHandler}
       />
 
-      <div className="main flex flex-row flex-wrap">
-        <div className="w-auto">
+      <div className="main flex flex-row">
+        <div className="w-auto flex flex-col">
           <textarea
-            className="w-72 bg-[#002B36] h-96 resize border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            className="w-72 h-[50vh] bg-[#002B36] resize border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             value={textAreaValue}
             onChange={changeTextAreaHandler}
           ></textarea>
+          <textarea
+            className="w-72 bg-[#002B36] h-[35vh] resize border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            value={textAreaVariable}
+            onChange={changeTextAreaVarHandler}
+          ></textarea>
+        </div>
+
+        <div className="w-full">
           <button
-            onClick={() => makeRequest(textAreaValue)}
+            onClick={() => makeRequestWithLibrary(textAreaValue)}
             className="absolute translate-x-[-50%] translate-y-[50%] w-[40px] h-[40px] border-black border-2 bg-sky-500 rounded-full hover:bg-green-500 transition-all duration-700"
           >
             <img className="mx-auto w-[20px] h-[20px]" src={play} alt="play" />
           </button>
-        </div>
-        <div className="min-w-min grow h-auto border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm">
-          {isLoading ? <Loader /> : <JsonView data={responseText} style={darkStyles} />}
+          <div className="min-w-min grow h-[85vh] overflow-auto border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm">
+            {isLoading ? <Loader /> : <JsonView data={responseText} style={darkStyles} />}
+          </div>
         </div>
       </div>
     </main>
