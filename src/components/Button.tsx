0a3e73f743ca7a2ba005bd44a13a63704f86e485
D1van007 <d1van28.01.1987@gmail.com>
2023-05-28 23:17:51 +0300

    fix: button whitespace-nowrap

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export function Button({ label, onClick, rowStart }: ButtonProps): JSX.Element {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 font-semibold row-start-${rowStart} w-full bg-sky-500 text-white hover:bg-green-500 transition-all duration-700`}
+      className={`px-4 py-2 whitespace-nowrap font-semibold row-start-${rowStart} w-full bg-sky-500 text-white hover:bg-green-500 transition-all duration-700`}
     >
       {label}
     </button>
